9f435643e47f350305805e755b7cc3da8b7a39b4
David Wampamba <davidwampamba@gmail.com>
2020-10-29 18:41:20 +0300

    Added Routes

--- src/App.js
+++ src/App.js
@@ -8,13 +8,19 @@ import {
 
 import Main from './components/navigation/main'
 import LoggedIn from './components/navigation/logged.in'
+
 import Home from './components/pages/home'
+import SignUP from './components/pages/sign.up'
+import ForgotPassword from './components/pages/forgot.password'
+import PrivateRoute from './components/config/private.route'
+
+import AddUser from './components/pages/add.user'
 import Users from './components/pages/users'
 import User from './components/pages/user'
+
 import AddNote from './components/pages/add.note'
 import Notes from './components/pages/notes'
 import NoteItem from './components/pages/note.item'
-import PrivateRoute from './components/config/private.route'
 
 const App = () => {
   return (
@@ -22,25 +28,26 @@ const App = () => {
         <Main />
         <LoggedIn />
         <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/new">
-            <AddNote />
-          </Route>
-          <Route path="/notes/:noteid">
-            <NoteItem />
-          </Route>
-          <Route path="/password">
-            <div>Change Password</div>
-          </Route>
-          <PrivateRoute path="/notes" component={Notes} />
-          <PrivateRoute path="/users" component={Users} />
-          <PrivateRoute path="/users/:id" component={User} />
-
           <Route path="*">
             <div>Not Found</div>
           </Route>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/sign-up">
+            <SignUP />
+          </Route>
+          <Route path="/forgot-password">
+            <ForgotPassword />
+          </Route>
+
+          <PrivateRoute path="/add/note" component={AddNote} />
+          <PrivateRoute path="/notes" component={Notes} />
+          <PrivateRoute path="/add/user" component={AddUser} />
+          <PrivateRoute path="/users" component={Users} />
+          <PrivateRoute path="/users/:id" component={User} />
+          <PrivateRoute path="/notes/:noteid" component={NoteItem} />
+
         </Switch>
     </Router>
   )
--- src/components/pages/password.js
+++ src/components/pages/password.js
@@ -1,8 +1,4 @@
 import React, {useState, useEffect} from 'react'
-import {
-    useRouteMatch,
-    useParams
-} from 'react-router-dom'
 
 const Password = () => {
 
@@ -13,4 +9,4 @@ const Password = () => {
     )
 }
 
-export default NoteItem
+export default Password
